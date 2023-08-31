19d7c6f6757061c7e9b13b76aae859613e819532
Cl0ne-a <63138126+Cl0ne-a@users.noreply.github.com>
2023-08-31 17:10:52 +0300

    Lecture 14 - 17 tasks

--- lecture_17/solitions/__process_big_volume_of_data.py
+++ lecture_17/solitions/__process_big_volume_of_data.py
@@ -5,27 +5,33 @@
 
 def text_breaker(function):
     def wrapper(path):
-        text = iter(function(path))
-        for line in text:
+        for line in function(path):
             yield line
     return wrapper
 
 
 @text_breaker
 def print_text(path):
-    my_file = open(path, "r")
-    text = my_file.read().split(".")
-    my_file.close()
-
-    return text
+    return iter(list(open(path, "r").read().split(". ")))
 
 
 print(next(print_text("text.txt")))
 print(next(print_text("text.txt")))
-print(next(print_text("text.txt")))
+# print(next(print_text("text.txt")))
 
 
 # OUTPUT:
 # This code defines a fun ction called plus_one that takes a single argument number and returns that number plus
 # This code defines a fun ction called plus_one that takes a single argument number and returns that number plus
 # This code defines a fun ction called plus_one that takes a single argument number and returns that number plus
+# text = iter(list(open("text.txt", "r").read().split(". ")))
+#
+#
+# def reader(some_text):
+#     for line in iter(some_text):
+#         yield line
+#
+#
+# print(next(reader(text)))
+# print(next(reader(text)))
+# print(next(reader(text)))
